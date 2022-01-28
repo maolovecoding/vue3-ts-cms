2d2544c65f71062b7e4b5ea1a1c49a2d9044eeb2
夜静听雨 <7710500+mao0826@user.noreply.gitee.com>
2022-01-28 22:41:45 +0800

    style(all): init
    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     "plugin:vue/vue3-essential",
@@ -9,12 +9,24 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    // eslint 兼容 prettier
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "@typescript-eslint/no-var-requires": "OFF",
+    "@typescript-eslint/no-empty-function": "OFF",
+    // 使用分号
+    semi: [0, "never"],
+    // 允许箭头函数不使用圆括号
+    "arrow-parens": 0,
+    // 使用any类型 不发生警告
+    "@typescript-eslint/no-explicit-any": "OFF",
+    // 允许参数使用any类型
+    "@typescript-eslint/explicit-module-boundary-types": "OFF"
+  }
 };
